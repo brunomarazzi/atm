3b8b10a9941bf5e9a5403d4a60d156ac3945a93d
Bruno Marazzi <brunomarazzidev@gmail.com>
2022-10-04 15:14:10 -0300

    new validations

---
+++ ATM/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public long CPF { get; set; }
+        public int Agency { get; set; }
+        public int Account { get; set; }
+    }
+}
--- ATM/Program.cs
+++ ATM/Program.cs
@@ -7,6 +7,18 @@ namespace ATM;
         // ATM Machine. My second project for practicing purposes using C#.
         static void Main()
         {
-            
+            Client client = new Client();
+        client.Name = "Bruno";
+        client.CPF = 66666666666;
+        client.Agency = 123;
+        client.Account = 456789;
+
+        Console.WriteLine(client.Name);
+        Console.WriteLine(client.CPF);
+        Console.WriteLine(client.Agency);
+        Console.WriteLine(client.Account);
+       
+        Console.ReadKey();
+
         }
     }
